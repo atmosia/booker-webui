ebbf54df77a51e90fb9e252551424dcc8551c87e
Magda Poppins <magdapoppins@Magdas-MacBook-Air.local>
2017-10-08 15:20:49 +0200

    Merge

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var app = angular.module("myApp", ["ngRoute"]);
+var app = angular.module("booker", ["ngRoute"]);
 app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
@@ -8,3 +8,5 @@ app.config(function($routeProvider) {
         templateUrl : "something.html"
     });
 });
+
+//ng-app name is booker
